3fb13b4040a85ddeee439c0e8558237756bf90df
smith-chris <kowalski.k@outlook.com>
2019-01-11 09:20:36 +0000

    Playing around with some types.

--- src/App.purs
+++ src/App.purs
@@ -5,6 +5,26 @@ import Prelude
 import Effect.Class (class MonadEffect)
 import Effect.Class.Console (log)
 
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+
+data Point = Point
+  { x :: Number
+  , y :: Number
+  }
+
+showPoint :: Point -> String
+showPoint (Point {x: x, y: y}) =
+  "(" <> show x <> ", " <> show y<> ")"
+
+showShape :: Shape -> String
+showShape (Circle c r) = "Circle with centre of " <> showPoint c <> ", and radius of " <> show r
+showShape (Rectangle p w h) = "Reactangle at position " <> showPoint p <> " with width of " <> show w <> "and height of " <> show h
+
+myCircle :: Shape
+myCircle = Circle (Point {x: 0.0, y: 0.0} ) 10.0
+
 main :: forall a b. MonadEffect b => a -> b Unit
 main time = do
-  log "Hello World!!!"
+  log $ showShape myCircle
